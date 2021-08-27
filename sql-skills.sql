25f53b5b8fe8d859f2582cd1b88a73d2de48675a
mckenjus <78286732+mckenjus@users.noreply.github.com>
2021-08-27 09:57:40 -0600

    Update sql-skills.sql

--- sql-skills.sql
+++ sql-skills.sql
@@ -57,3 +57,44 @@ SELECT al.title, ar.name
 FROM album al
 JOIN artist ar
 ON al.artist_id = ar.artist_id;
+
+SELECT * FROM artist ORDER BY name DESC LIMIT 10;
+
+SELECT * FROM artist WHERE name LIKE 'Black%';
+
+SELECT * FROM artist WHERE name LIKe '%Black%'
+
+SELECT * FROM employee;
+
+SELECT MAX(birth_date) FROM employee;
+
+SELECT MIN (birth_date) FROM employee;
+
+SELECT *
+FROM invoice
+WHERE billing_state IN ('TX', 'CA', 'AZ');
+
+SELECT AVG(total) FROM invoice;
+
+SELECT pt.track_id, p.name
+FROM playlist_track pt
+JOIN playlist p
+ON pt.playlist_id = p.playlist_id WHERE name = 'Music';
+
+select t.name AS track, pt.playlist_track_id
+FROM track t
+JOIN playlist_track pt
+ON t.track_id = pt.track_id WHERE playlist_track_id = 5;
+
+
+SELECT t.name AS track, p.name AS playlist FROM track t
+JOIN playlist_track pt
+ON pt.track_id = t.track_id
+JOIN playlist p
+ON p.playlist_id = pt.playlist_id;
+
+SELECT t.name AS track, a.title AS album FROM album a
+JOIN track t
+ON t.album_id = a.album_id
+JOIN genre g
+ON g.genre_id = t.genre_id WHERE g.name = 'Alternative & Punk'
